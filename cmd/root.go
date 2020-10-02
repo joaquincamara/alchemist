1c132612c4929f3c54c0a7a748bcf8823fa28113
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-10-02 10:09:08 -0500

    src files creation added

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -40,13 +25,8 @@ var rootCmd = &cobra.Command{
 \____|__  /|____/ \___  >|___|  / \___  >|__|_|  /|__|/____  > |__|  
         \/            \/      \/      \/       \/          \/
    `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -57,18 +37,11 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.alchemist.yaml)")
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
--- cmd/transmute.go
+++ cmd/transmute.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -21,10 +6,11 @@ import (
 	"log"
 	"os"
 
+	"alchemist/reactContent"
+
 	"github.com/spf13/cobra"
 )
 
-// transmuteCmd represents the transmute command
 var transmuteCmd = &cobra.Command{
 	Use:   "transmute",
 	Short: "Init the setup configuration for a alchemist project",
@@ -36,63 +22,91 @@ var transmuteCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(transmuteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// transmuteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// transmuteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 func createReactApp() {
-	fmt.Print("Enter the name of you app: ")
 	var appName string
+
+	// Project base files
+	var jsonContent = reactContent.PackageJsonContentGenerator()
+	var readmeContent = reactContent.ReadmeContentGenerator()
+	var gitIgnoreContent = reactContent.GitIgnoreContentGenerator()
+
+	//SRC files
+
+	var appCssContent = reactContent.AppCssContentGenerator()
+	var appJsContent = reactContent.AppJsContentGenerator()
+	var appTestJsContent = reactContent.AppTestJsContentGenerator()
+	var indexCssContent = reactContent.IndexCssContentGenerator()
+	var indexJsContent = reactContent.IndexJsContentGenerator()
+	var logoSvgContent = reactContent.LogoSvgContentGenerator()
+	var serviceWorkerContent = reactContent.ServiceWorkerContentGenerator()
+	var setUpTestContent = reactContent.SetUpTestContentGenerator()
+
+	fmt.Print("Enter the name of you app: ")
 	fmt.Scanln(&appName)
-	//fmt.Print(appName)
+
 	os.Mkdir(appName, 0755)
 	os.Mkdir(appName+"/src", 0755)
 	os.Mkdir(appName+"/puclic", 0755)
-	message := []byte(`{
-		"name": "my-app",
-		"version": "0.1.0",
-		"private": true,
-		"dependencies": {
-			"@testing-library/jest-dom": "^4.2.4",
-			"@testing-library/react": "^9.3.2",
-			"@testing-library/user-event": "^7.1.2",
-			"react": "^16.13.1",
-			"react-dom": "^16.13.1",
-			"react-scripts": "3.4.3"
-		},
-		"scripts": {
-			"start": "react-scripts start",
-			"build": "react-scripts build",
-			"test": "react-scripts test",
-			"eject": "react-scripts eject"
-		},
-		"eslintConfig": {
-			"extends": "react-app"
-		},
-		"browserslist": {
-			"production": [
-				">0.2%",
-				"not dead",
-				"not op_mini all"
-			],
-			"development": [
-				"last 1 chrome version",
-				"last 1 firefox version",
-				"last 1 safari version"
-			]
-		}
+
+	// Project base files creation
+
+	jsonContentErr := ioutil.WriteFile(appName+"/package.json", jsonContent, 0644)
+	if jsonContentErr != nil {
+		log.Fatal(jsonContentErr)
 	}
-	`)
-	err := ioutil.WriteFile(appName+"package.json", message, 0644)
-	if err != nil {
-		log.Fatal(err)
+
+	readmeContentErr := ioutil.WriteFile(appName+"/README.md", readmeContent, 0644)
+	if readmeContentErr != nil {
+		log.Fatal(readmeContentErr)
 	}
+
+	gitIgnoreContentErr := ioutil.WriteFile(appName+"/.gitignore", gitIgnoreContent, 0644)
+	if gitIgnoreContentErr != nil {
+		log.Fatal(gitIgnoreContentErr)
+	}
+
+	// SRC files creation
+
+	appCssContentContentErr := ioutil.WriteFile(appName+"/src/App.css", appCssContent, 0644)
+	if appCssContentContentErr != nil {
+		log.Fatal(appCssContentContentErr)
+	}
+
+	appJsContentErr := ioutil.WriteFile(appName+"/src/App.js", appJsContent, 0644)
+	if appJsContentErr != nil {
+		log.Fatal(appJsContentErr)
+	}
+
+	appTestJsContentErr := ioutil.WriteFile(appName+"/src/App.test.js", appTestJsContent, 0644)
+	if appTestJsContentErr != nil {
+		log.Fatal(appTestJsContentErr)
+	}
+
+	indexCssContentErr := ioutil.WriteFile(appName+"/src/index.css", indexCssContent, 0644)
+	if indexCssContentErr != nil {
+		log.Fatal(indexCssContentErr)
+	}
+
+	indexJsContentErr := ioutil.WriteFile(appName+"/src/index.js", indexJsContent, 0644)
+	if indexCssContentErr != nil {
+		log.Fatal(indexJsContentErr)
+	}
+
+	logoSvgContentErr := ioutil.WriteFile(appName+"/src/logo.svg", logoSvgContent, 0644)
+	if logoSvgContentErr != nil {
+		log.Fatal(logoSvgContentErr)
+	}
+
+	serviceWorkerContentErr := ioutil.WriteFile(appName+"/src/serviceWorker.js", serviceWorkerContent, 0644)
+	if serviceWorkerContentErr != nil {
+		log.Fatal(serviceWorkerContentErr)
+	}
+
+	setUpTestContentErr := ioutil.WriteFile(appName+"/src/setupTests.js", setUpTestContent, 0644)
+	if setUpTestContentErr != nil {
+		log.Fatal(setUpTestContentErr)
+	}
+
 }
