77945e09146f1e7ef0901fbd58c656b2acd287df
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-10-03 09:53:30 -0500

    adding web server functionalities

---
+++ server/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Wellcome fellow alchemist!")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
