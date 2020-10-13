cd2a0e56995ef157290f4c7e302131a124942504
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-10-12 21:31:55 -0500

    fixing and adding main.ts at src dir

---
+++ nestContent/mainTs.go
@@ -0,0 +1,17 @@
+package nestContent
+
+func MainTs() []byte {
+	mainTs := []byte(`
+	import { NestFactory } from '@nestjs/core';
+    import { AppModule } from './app.module';
+
+    async function bootstrap() {
+       const app = await NestFactory.create(AppModule);
+       await app.listen(3000);
+    }
+    bootstrap();
+
+	`)
+
+	return mainTs
+}
