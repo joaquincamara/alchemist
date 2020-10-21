13c3087b5324e72e6f8ab9d95b77a03c9d6eb820
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-10-21 14:23:43 -0500

    creating a mox router

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module server
 
 go 1.15
+
+require github.com/gorilla/mux v1.8.0
--- server/server.go
+++ server/server.go
@@ -1,50 +1,22 @@
 package main
 
 import (
-	"flag"
-	"html/template"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
-
-var templ = template.Must(template.New("png").Parse(templateStr))
+//
+func homeRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome fellow Alchemist to the ApiRest transmutation")
+}
 
 func main() {
+	router := mux.NewRouter().StrictSlash(true)
 
-	initServer()
+	router.HandleFunc("/", homeRoute)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
-func initServer() {
-	flag.Parse()
-	http.Handle("/", http.HandlerFunc(png))
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-}
-
-func png(w http.ResponseWriter, req *http.Request) {
-	templ.Execute(w, req.FormValue("s"))
-}
-
-const templateStr = `
-<html>
-<head>
-<title>QR Link Generator</title>
-</head>
-<body>
-{{if .}}
-<img src="https://user-images.githubusercontent.com/26718123/94979651-96461b00-04e9-11eb-94d6-660af9663975.{{.}}" />
-<br>
-<br>
-<br>
-{{end}}
-<form action="/" name=f method="GET">
-    <input maxLength=1024 size=70 name=s value="" title="Text to QR Encode">
-    <input type=submit value="Show QR" name=qr>
-</form>
-</body>
-</html>
-`
