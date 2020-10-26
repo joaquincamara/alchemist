b13fcec858f68ac2c66bb22407dc46311ec13ddf
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2020-10-25 19:40:47 -0600

    saving the server module

---
+++ server/api-server.go
@@ -0,0 +1,15 @@
+package server
+
+// Creates the standar code for a alchemist RestApi
+func AppServer() []byte {
+	appServer := []byte(`
+	package main
+
+	import (
+		
+	)
+ 
+  `)
+
+	return appServer
+}
