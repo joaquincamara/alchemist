ff8e9bf9a06ee968c81be74b36a253c0764bff5b
Joaquin Camara <joaquin.camara.rivera@gmail.com>
2021-07-27 11:51:02 -0500

    server package updatted

--- server/createAlchemistServer.go
+++ server/createAlchemistServer.go
@@ -6,10 +6,25 @@ import (
 	"os"
 )
 
+var serverTemplate = []byte(`
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/joaquincamara/silver"
+)
+
+func main() {
+	router := silver.NewRouter()
+	router.GET("/", silver.HomeRoute)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+`)
+
 func CreateAlchemistServer(appName string) {
-	var appServer = ApiServer()
 	os.Mkdir(appName, 0755)
-	appServerErr := ioutil.WriteFile(appName+"/server.go", appServer, 0644)
+	appServerErr := ioutil.WriteFile(appName+"/main.go", serverTemplate, 0644)
 	if appServerErr != nil {
 		log.Fatal(appServerErr)
 	}
